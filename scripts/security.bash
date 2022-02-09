25aa5ad23aeeb33f20da1fd27cb8c9517a20785b
Aidan O'Connor <89805687+aidan-fugue@users.noreply.github.com>
2022-02-09 14:41:23 -0500

    Update security.bash

--- scripts/security.bash
+++ scripts/security.bash
@@ -25,5 +25,6 @@ if [[ $rc != 0 ]]; then
   else 
     echo -e "\n${green}Security and compliance check passed successfully! Moving on to the next step in the Scalr pipeline.${clear}\n"
     #(Optional) Send results of security and compliance scan to Fugue UI
-    ./regula run --sync --upload; exit 0
+    #./regula run --sync --upload; exit 0
+    ./regula run; exit 0
   fi
